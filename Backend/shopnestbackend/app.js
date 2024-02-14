d7865387bf7038c0b503daf584244247d013ccb5
Anandraj2501 <araj250101@gmail.com>
2024-02-14 20:11:48 +0530

    First

--- Backend/shopnestbackend/app.js
+++ Backend/shopnestbackend/app.js
@@ -33,7 +33,7 @@ app.use(cors({
 }));
 
 // Allow access to static images
-app.use('/images', express.static(path.join(__dirname, 'public/images')));
+// app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 
 app.use(session({
